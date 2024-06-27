4b9a996a4616ab77a2104639170c4a7f6482d497
Christian Vedel <christian.vedel@ess.eu>
2024-06-27 15:13:10 +0200

    DescriptorNumber constructor test and check tests

--- tests/unit_tests/Objects/new_variable/test_descriptor_bool.py
+++ tests/unit_tests/Objects/new_variable/test_descriptor_bool.py
@@ -51,7 +51,7 @@ class TestDescriptorBool:
         # Expect
         assert descriptor._bool_value == False
 
-    @pytest.mark.parametrize("bool_value", ["string", 0, 1.0])
+    @pytest.mark.parametrize("bool_value", ["string", 0, 0.0])
     def test_set_value_type_exception(self, descriptor: DescriptorBool, bool_value):
         # When Then Expect
         with pytest.raises(TypeError):
