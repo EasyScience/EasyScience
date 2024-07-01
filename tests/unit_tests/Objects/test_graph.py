d2e7edfd0e3ba4700076096d9de2783d358e3c70
Christian Vedel <christian.vedel@ess.eu>
2024-07-01 16:26:05 +0200

    Clear graph unittest

--- tests/unit_tests/Objects/test_graph.py
+++ tests/unit_tests/Objects/test_graph.py
@@ -1,7 +1,26 @@
-__author__ = "github.com/wardsimon"
-__version__ = "0.1.0"
-
 #  SPDX-FileCopyrightText: 2023 EasyScience contributors  <core@easyscience.software>
 #  SPDX-License-Identifier: BSD-3-Clause
 #  © 2021-2023 Contributors to the EasyScience project <https://github.com/easyScience/EasyScience
+
+from easyscience.Objects.Graph import Graph
+from easyscience.Objects.Variable import Parameter
+from easyscience.Objects.ObjectClasses import BaseObj
+import pytest
+from easyscience import borg
+
+class TestGraph:
+    @pytest.fixture
+    def clear(self):
+        borg.map._clear()
+
+    def test_clear(self):
+        borg.map._clear()
+        test_obj = BaseObj("test")
+        assert len(borg.map._store) == 1
+        assert len(borg.map._Graph__graph_dict) == 1
+        assert borg.map._name_iterator_dict == {"BaseObj": 0}
+        borg.map._clear()
+        assert len(borg.map._store) == 0
+        assert borg.map._Graph__graph_dict == {}
+        assert borg.map._name_iterator_dict == {}
 
