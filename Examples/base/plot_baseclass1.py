ebbfae4a605aca6f9727e7f6cd4b57c29bfb0781
rozyczko <piotr.rozyczko@gmail.com>
2024-08-15 15:10:41 +0200

    modify to windows

--- Examples/base/plot_baseclass1.py
+++ Examples/base/plot_baseclass1.py
@@ -16,6 +16,7 @@ saving time from multiple imports.
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 from easyscience.Objects.ObjectClasses import BaseObj
 from easyscience.Objects.ObjectClasses import Parameter
 
