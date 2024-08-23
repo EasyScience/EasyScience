009f054dd24649725c32c1ac5ab7371c66f1fd2e
Andreas Pedersen <andreas.pedersen@ess.eu>
2024-08-23 14:00:29 +0200

    expose available minimizers in fitter module

--- src/easyscience/fitting/__init__.py
+++ src/easyscience/fitting/__init__.py
@@ -1,7 +1,8 @@
 from .fitter import Fitter
+from .minimizers.factory import AvailableMinimizers
 from .minimizers.utils import FitResults
 
 # Causes circular import
 # from .multi_fitter import MultiFitter  # noqa: F401, E402
 
-all = [Fitter, FitResults]
+all = [AvailableMinimizers, Fitter, FitResults]
